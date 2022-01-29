192678740bef4d8441cbcb31357b639800e1c060
the company <thecompany001@protonmail.com>
2022-01-29 12:13:30 -0500

    leet prac

---
+++ oop_prac_2.py
@@ -0,0 +1,45 @@
+class Pet:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old.")
+        
+    def speak(self):
+        print("I don't know what to say.")
+
+
+class Cat(Pet):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
+        
+    def speak(self):
+        print("Meow")
+        
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old and I am {self.color}.")
+        
+class Dog(Pet):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def speak(self):
+        print("Bark")
+
+class Fish(Pet):
+    pass
+        
+p = Pet("Tim", 9)
+p.show()
+p.speak()
+c = Cat("Bill", 34, "Brown")
+c.show()
+c.speak()
+d = Dog("Jill", 25)
+d.show()
+d.speak()
+f = Fish("Bubbles", 10)
+f.speak()
